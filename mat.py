c37b50483afb01da2318a4c15e4e5f4118ddd72a
Samir Karimov <trinusd@gmail.com>
2024-02-06 14:09:50 +0400

    Create mat.py

---
+++ mat.py
@@ -0,0 +1,8 @@
+def scalar_multiply(matrix, scalar):
+    pass
+
+
+# Example usage
+matrix_c = [[2, 4, 6], [8, 10, 12], [14, 16, 18]]
+scalar_value = 3
+result_scalar_multiply = scalar_multiply(matrix_c, scalar_value)
